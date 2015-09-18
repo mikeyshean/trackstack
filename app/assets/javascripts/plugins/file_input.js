7e818be75a8521db595dfb882b9d7b61c0fd8036
Mikey Shean <mdshean2@gmail.com>
2015-09-18 00:53:53 -0400

    Working State from prev.

--- app/assets/javascripts/plugins/file_input.js
+++ app/assets/javascripts/plugins/file_input.js
@@ -1,24 +1,23 @@
-(function () {
-  $.FileInput = function (el) {
-    this.$el = $(el);
-
-    this.$el.on("click", "#css-file-input", this.showUploadModal.bind(this));
-  };
-
-  $.FileInput.prototype.showUploadModal = function (e) {
-    debugger
-    e.preventDefault();
-    $(e.currentTarget).find("#file-input-button").click();
-  };
-
-  $.fn.fileinput = function () {
-    return this.each(function () {
-      new $.FileInput(this);
-    });
-  };
-
-})();
-
-$(function () {
-  $(".file-input-plugin").fileinput();
-});
+// (function () {
+//   $.FileInput = function (el) {
+//     this.$el = $(el);
+//
+//     this.$el.on("click", "#css-file-input", this.showUploadModal.bind(this));
+//   };
+//
+//   $.FileInput.prototype.showUploadModal = function (e) {
+//     e.preventDefault();
+//     $(e.currentTarget).find("#file-input-button").click();
+//   };
+//
+//   $.fn.fileinput = function () {
+//     return this.each(function () {
+//       new $.FileInput(this);
+//     });
+//   };
+//
+// })();
+//
+// $(function () {
+//   $(".file-input-plugin").fileinput();
+// });
