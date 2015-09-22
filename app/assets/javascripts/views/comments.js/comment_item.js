8c73de3c8254146b617ff25a65323263c3a4babb
Mikey Shean <mdshean2@gmail.com>
2015-09-22 17:37:21 -0400

    Render Comments on TrackShow.

--- app/assets/javascripts/views/comments.js/comment_item.js
+++ app/assets/javascripts/views/comments.js/comment_item.js
@@ -1,8 +1,9 @@
 Trackstack.Views.CommentItem = Backbone.CompositeView.extend({
   template: JST['comments/comment'],
-
+  tagName: "li",
 
   render: function () {
+
     this.$el.html(this.template({ comment: this.model }))
     return this;
   }
--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -20,7 +20,7 @@ module Api
     def show
       @track = Track.includes(:likers, comments: :author).where(id: params[:id]).first
 
-      render partial: "track", locals: { track: @track }
+      render :show
     end
 
     def update
