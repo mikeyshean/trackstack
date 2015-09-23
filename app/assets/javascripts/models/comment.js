249b66f24efacdcfd2f3b150190cb3ead6a05f3a
Mikey Shean <mdshean2@gmail.com>
2015-09-22 23:27:18 -0400

    Add comment submitted_at time conversion.

--- app/assets/javascripts/models/comment.js
+++ app/assets/javascripts/models/comment.js
@@ -7,7 +7,29 @@ Trackstack.Models.Comment = Backbone.Model.extend({
     return { "comment": _.clone(this.attributes) }
   },
 
-  convertTime: function () {
-    this.submitted_at
+  convertedTime: function () {
+    var s = this.get("submitted_at");
+
+    var hours = Math.floor(s / 3600)
+    s -= hours * 3600
+
+    var minutes = Math.floor(s / 60)
+    s -= minutes * 60
+
+    s = Math.floor(s)
+
+    if (hours >= 1) {
+      return this._padded(hours) + ":" + this._padded(minutes) + ":" + this._padded(s)
+    } else {
+      return this._padded(minutes) + ":" + this._padded(s)
+    }
+  },
+
+  _padded: function(num) {
+    if (num >= 1) {
+      return num;
+    } else {
+      return "0" + num
+    }
   }
 });
