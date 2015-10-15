d7a4bf4fe6cf7ef02413c349a9fb66d95974fa4b
Mikey Shean <mdshean2@gmail.com>
2015-10-15 01:14:57 -0400

    Update followers_controller#create.

--- app/controllers/api/followers_controller.rb
+++ app/controllers/api/followers_controller.rb
@@ -5,7 +5,7 @@ module Api
       following = Following.new({follower_id: current_user.id, followee_id: params[:followee_id]})
 
       if following.save
-        render json: { id: following.followee_id }
+        render json: { id: current_user.id }
       else
         render json: following.errors.full_messages, status: 422
       end
